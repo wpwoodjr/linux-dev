240752bf4ed76b41f0c2658841f855d35d02c753
Bill Wood <wpwoodjr@gmail.com>
2022-05-22 21:45:07 -0400

    added support for setting timezone on first run

--- run.sh
+++ run.sh
@@ -14,6 +14,6 @@ WORK_DIR="/home/$USER/work"
 set -e
 
 sudo -E "$(which docker)" run -itd --restart=always --hostname "$NAME" --name "$NAME" --env HOST_DIR="$HOST_DIR" \
-  -v "$HOST_DIR":"$WORK_DIR" -v /var/run/docker.sock:/var/run/docker.sock $@ linux-dev
+  -v "$HOST_DIR":"$WORK_DIR" -v /var/run/docker.sock:/var/run/docker.sock $@ linux-dev /bin/bash --norc
 
 echo "Container \"$NAME\" running with host directory \"$HOST_DIR\" mounted at \"$WORK_DIR\""
