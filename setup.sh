95272ecd0f405062d7c7e4c4b22fc6b545ecd139
Bill Wood <wpwoodjr@gmail.com>
2023-01-14 16:44:54 -0500

    release 3 alpha 2

--- setup.sh
+++ setup.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 # linux-dev setup.sh
 
-# set umask
-echo -e "\n===========> setting umask to u=rwx,g=rx,o=rx..."
-umask u=rwx,g=rx,o=rx
+# registers a tool as installed
+installed_tools="$HOME/.linux-dev/installed-tools"
+register_installed() {
+    echo -e "\n===========> installing $1..."
+    [ "$EUID" -ne 0 ] && grep -s -q "^$1$" "$installed_tools" || echo "$1" >>"$installed_tools"
+}
 
+register_installed "setup"
 # install utilities, init, base
 echo -e "\n===========> installing utility tools..."
-sudo cp source/install-tools source/merge-kubeconfig source/az-get-credentials /usr/local/bin/
 for f in "install-tools" "merge-kubeconfig" "az-get-credentials"; do
+  sudo cp "source/$f" /usr/local/bin
   sudo chmod a+rx-w "/usr/local/bin/$f"
   # sed command converts Windows (CR LF) to Unix (LF) line endings
   sudo sed -i 's/\r$//' "/usr/local/bin/$f"
   ls -l "/usr/local/bin/$f"
 done
+grep -s -q "### linux-dev ###" ~/.bashrc || echo -e "\n### linux-dev ###\nalias update-tools=\"install-tools upgrade\"" >>~/.bashrc
+
 install-tools init base $@
+
+echo -e "\n===========> finished setup\numask is $(umask -S)\nlocale is $LANG"
